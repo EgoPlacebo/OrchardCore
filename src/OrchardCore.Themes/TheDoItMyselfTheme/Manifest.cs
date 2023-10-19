b0de17ade3dc0cb101cfaacf960dafdc88a31eb6
Ego Placebo <ej.benzie@gmail.com>
2023-10-19 08:11:31 +0800

    basic working theme

---
+++ src/OrchardCore.Themes/TheDoItMyselfTheme/Manifest.cs
@@ -0,0 +1,11 @@
+using OrchardCore.DisplayManagement.Manifest;
+using OrchardCore.Modules.Manifest;
+
+[assembly: Theme(
+    Name = "The Do It Myself Theme",
+    Author = "Ego Placebo",
+    Website = "",
+    Version = "1.7.1",
+    Description = "Based on the Default Theme.",
+    Tags = new[] { "Bootstrap", "Default" }
+)]
